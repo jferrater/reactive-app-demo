69f0208348898d53aef6be25f6b8de5b194da3d3
Joffry Ferrater <joffry.ferrater@gmail.com>
2024-02-22 16:25:41 +0100

    - change to spring boot 2.7+ - add embedded mongodb for tests

--- payment-service/src/test/java/com/github/jferrater/paymentservice/PaymentServiceApplicationTests.java
+++ payment-service/src/test/java/com/github/jferrater/paymentservice/PaymentServiceApplicationTests.java
@@ -1,10 +1,16 @@
 package com.github.jferrater.paymentservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PaymentServiceApplicationTests {
+
+	// fixing bean creation in test with name 'embeddedMongoServer'
+	static {
+		System.setProperty("spring.mongodb.embedded.version","5.0.0");
+	}
 
 	@Test
 	void contextLoads() {
