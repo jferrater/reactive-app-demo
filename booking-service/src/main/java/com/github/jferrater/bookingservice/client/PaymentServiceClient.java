dc0732662f82a9a586423234e77c051594ed159a
Joffry Ferrater <joffry.ferrater@trioptima.com>
2024-02-23 17:20:57 +0100

    - payment service client implementation - integration test

--- booking-service/src/main/java/com/github/jferrater/bookingservice/client/PaymentServiceClient.java
+++ booking-service/src/main/java/com/github/jferrater/bookingservice/client/PaymentServiceClient.java
@@ -1,4 +1,30 @@
 package com.github.jferrater.bookingservice.client;
 
+
+import com.github.jferrater.bookingservice.dto.TransactionRequest;
+import com.github.jferrater.bookingservice.dto.TransactionResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
 public class PaymentServiceClient {
+
+    private final WebClient webClient;
+
+    public PaymentServiceClient(@Value("${payment-service.base-url}") String url) {
+        this.webClient = WebClient.builder()
+                .baseUrl(url)
+                .build();
+    }
+
+    public Mono<TransactionResponse> authorizeTransaction(TransactionRequest transactionRequest) {
+        return this.webClient
+                .post()
+                .uri("authorize_transaction")
+                .bodyValue(transactionRequest)
+                .retrieve()
+                .bodyToMono(TransactionResponse.class);
+    }
 }
